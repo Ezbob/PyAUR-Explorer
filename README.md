52ec11e1ce1ea4cbc0c7e85c063a4222938559fd
Ezbob <andersbusch@gmail.com>
2016-07-10 17:53:56 +0200

    Fixed a bug...:P

--- README.md
+++ README.md
@@ -5,7 +5,15 @@ This a Arch User Repository explorer written in Python 2.
 With this you can search for different packages located in the AUR.
 
 ## How-To
+
+### Run it
 To search for a package simply use the following command:
 ``` python main.py <package query>```
 
-Where `<package query>` can be replace with the name of the package you're looking for. 
+Where `<package query>` can be replace with the name of the package you're looking for.
+
+### Get help
+To get an overview over the options you can parse to the explorer:
+```python main.py --help``` or
+```python main.py -h ``` 
+This will give you a short usage message.
--- main.py
+++ main.py
@@ -9,10 +9,12 @@ AUR_URL = "https://aur.archlinux.org"
 RPC_URL = AUR_URL + "/rpc"
 
 ENTRIES_SHOWN = 4
+LONG_PRINT = False
 
 def argument_parse():	
 	parser = argparse.ArgumentParser(description="Get stuff from the Arch User Repository")
 	parser.add_argument("pkg_name", help="Package name")
+	parser.add_argument("-lp","--long-print", help="Print all info fields")
 	return parser.parse_args()
 
 def open_get_request(url, params):
@@ -29,18 +31,26 @@ def search_aur( pkg_name ):
 	params = { 'v' : '5' }
 	params['type'] = 'search'
 	params['arg'] = pkg_name
-	json = check_json( open_get_request( rpc_url, params ) )
+	json = check_json( open_get_request( RPC_URL, params ) )
 	return json['resultcount'], json['results']
 
-def print_entry(entry):
-	if entry['Name'] != None:
+def print_entry(entry, long_version=False):
+	if long_version:
 		print entry['Name']
-	if entry['Description'] != None:
-		print "\t\"" + entry['Description'] + "\""
-	if entry['Maintainer'] != None:
-		print "\tMaintainer: " + entry['Maintainer']
-	if entry['Version'] != None: 
-		print "\tVersion: " + entry['Version']
+		for key, val in entry.iteritems():
+			if key == 'Name':
+				pass
+			else:
+				print "\t" + key + ": " + str(val)
+	else:
+		if entry['Name'] != None:
+			print entry['Name']
+		if entry['Description'] != None:
+			print "\t\"" + entry['Description'] + "\""
+		if entry['Maintainer'] != None:
+			print "\tMaintainer: " + entry['Maintainer']
+		if entry['Version'] != None: 
+			print "\tVersion: " + entry['Version']
 
 if __name__ == '__main__':
 	args = argument_parse()
@@ -64,7 +74,7 @@ if __name__ == '__main__':
 		else:
 			print "Could not find a direct match. Found alternatives:"
 			for index in range( min( count, ENTRIES_SHOWN ) ):
-				print_entry(results[index])
+				print_entry(results[index], True)
 
 #	dirpath = tempfile.mkdtemp()
 #	os.chdir( dirpath )
